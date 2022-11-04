01d171b8f6128b91df87928b3142d5d9f364e6ad
Gunichi <gunichi297@gmail.com>
2022-11-04 17:17:58 -0300

    responsive

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Box, Text, Button, Flex, HStack, Image, SimpleGrid, Container } from '@chakra-ui/react'
-
-import SmallWithLogoLeft from '../../components/Footer/Footer'
 
 import Faq from '../../components/Faq/Faq'
 import Features from '../../components/Feature/Feature'
+import SmallWithLogoLeft from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 import WithSubnavigation from '../../components/Navbar/Navbar'
 
